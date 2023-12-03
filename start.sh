134407e0f17473714534d25f2ea1cb99b637c419
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2023-12-03 14:02:53 +0100

    Add JetStream nats to docker-compose.yaml, and add gravity env variables

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-docker ps -aq | xargs docker stop | xargs docker rm
+# docker ps -aq | xargs docker stop | xargs docker rm
 
 export STAR_HOSTNAME=star
-export STAR_PORT=9000
+export STAR_PORT=11000
 export STAR_HTTP_PORT=7777
 export MAGNETAR_HOSTNAME=magnetar
 export MAGNETAR_PORT=5000
@@ -13,16 +13,30 @@ export NATS_HOSTNAME=nats
 export NATS_PORT=4222
 export ETCD_HOSTNAME=etcd
 export ETCD_PORT=2379
-export BLACKHOLE_HOSTNAME=queue
-export BLACKHOLE_PORT=50051
+export AGENT_QUEUE_HOSTNAME=agent_queue
+export AGENT_QUEUE_PORT=50052
+export IAM_HOSTNAME=iam-service
+export IAM_PORT=8002
 
-export DB_PASSWORD=c12s_password
-export DB_USERNAME=postgres
-export DB_NAME=postgres
-export DB_HOST=database
-export DB_PORT=5432
+export NODES_NUM=2
+export REGISTRATION_REQ_TIMEOUT_MILLISECONDS=1000
+export MAX_REGISTRATION_RETRIES=5
 
-export BLACKHOLE_GRPC_PORT=50051
+export STAR_PORT_END=$(($STAR_PORT + $NODES_NUM - 1))
+
+# port range for multiple star instances
+# todo: each instance should have a dedicated volume
+# we probably won't be able to use docker-compose anymore
+# since it doesn't support this feature
+if [ -z "$STAR_PORT" ]; then
+  echo "$STAR_PORT is not set. Please set it and try again."
+  exit 1
+fi
+if [ -z "$NODES_NUM" ]; then
+  echo "$NODES_NUM is not set. Please set it and try again."
+  exit 1
+fi
+
 
 export REGISTRATION_SUBJECT="register"
 export REGISTRATION_REQ_TIMEOUT_MILLISECONDS=1000
@@ -42,4 +56,4 @@ export NEO4J_apoc_import_file_use__neo4j__config=true
 export NEO4J_PLUGINS="[\"apoc\"]"
 
 docker compose build
-docker compose up
+docker-compose up --scale star=${NODES_NUM}
